8f7f9b1330d26d31fb2147b66e9b222ecb5436aa
marowsky <peter.marowsky@gmail.com>
2017-11-08 00:04:23 +0100

    aaa

--- app/src/main/java/de/petesky/motorlist/ItemListActivity.java
+++ app/src/main/java/de/petesky/motorlist/ItemListActivity.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView;
 import de.petesky.motorlist.dummy.DummyContent;
 import java.util.List;
